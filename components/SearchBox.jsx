3759ecd7409632af3aa459225e2a6dc7321e254f
David Kurnia Kristiadi <davidkurniakristiadi@gmail.com>
2017-07-30 02:13:55 +0700

    Capitalized main navigation items.

--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 class SearchBox extends React.Component {
 	render() {
 		return (
-			<div className="search-box {this.props.button ? search-box-with-submit : null}">
+			<form className="search-box {this.props.button ? search-box-with-submit : null}">
 				<input type="text" placeholder={this.props.placeholder} />
 				{this.props.button ? <button role="submit">{this.props.button}</button> : null }
-			</div>
+			</form>
 		);
 	}
 }
