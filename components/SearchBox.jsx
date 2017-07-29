e9f2bed3357fc9eaa4267c25ccde374bd6ce9b32
David Kurnia Kristiadi <davidkurniakristiadi@gmail.com>
2017-07-30 02:25:09 +0700

    Added function to SearhBox component to handle button disabled status.

--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
 class SearchBox extends React.Component {
+	constructor() {
+		super();
+		this.handleChange = this.handleChange.bind(this);
+		this.state = {
+			searchText: ''
+		}
+	}
+
 	render() {
 		return (
-			<form className="search-box {this.props.button ? search-box-with-submit : null}">
-				<input type="text" placeholder={this.props.placeholder} />
-				{this.props.button ? <button role="submit">{this.props.button}</button> : null }
+			<form className="search-box">
+				<input type="text" value={this.state.searchText} placeholder={this.props.placeholder} onChange={this.handleChange} />
+				{this.props.button ? <button role="submit" disabled={ this.state.searchText ? false : true }>{this.props.button}</button> : null }
 			</form>
 		);
+	}
+
+	handleChange(e) {
+		this.setState({
+			searchText: e.target.value
+		})
 	}
 }
 
